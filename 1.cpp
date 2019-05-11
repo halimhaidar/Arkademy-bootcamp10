2a8ce0e867aa2357359e241254d067816a9ff765
Halim Haidar <halimpiar06@gmail.com>
2019-05-11 12:11:52 +0700

    add hobbies

--- 1.cpp
+++ 1.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include<bits/stdc++.h>
+
 using namespace std;
+
 int main(){
+
 char nm[20]="ABDUL HALIM HAIDAR";
+char hb[2] [10]= {"Playing", "Blogging"};
 
-cout<<"Name  : "<<nm<<endl;
+cout<<"Name   : "<< nm << endl;
 
-
+for (int i = 0; i < 2; i++)  
+        cout << "Hobbies: "<< hb[i] << endl;
 
 return 0;
 }
