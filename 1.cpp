de635a85c3d83d7e4ecad282201c93b52058bb65
Halim Haidar <halimpiar06@gmail.com>
2019-05-11 12:19:41 +0700

    add address

--- 1.cpp
+++ 1.cpp
@@ -6,9 +6,11 @@ using namespace std;
 int main(){
 
 char nm[20]="ABDUL HALIM HAIDAR";
+string add ="Padang/West Sumatera/Indonesia";
 char hb[2] [10]= {"Playing", "Blogging"};
 
 cout<<"Name   : "<< nm << endl;
+cout<<"Address: "<< add << endl;
 
 for (int i = 0; i < 2; i++)  
         cout << "Hobbies: "<< hb[i] << endl;
