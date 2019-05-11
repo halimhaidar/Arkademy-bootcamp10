a3e907d968e8c5aa38bea1351112d73ebd967212
Halim Haidar <halimpiar06@gmail.com>
2019-05-11 12:53:05 +0700

    final

--- 1.cpp
+++ 1.cpp
@@ -4,16 +4,22 @@
 using namespace std;
 
 int main(){
-
+int i;
 char nm[20]="ABDUL HALIM HAIDAR";
 string add ="Padang/West Sumatera/Indonesia";
 char hb[2] [10]= {"Playing", "Blogging"};
+bool mar=false;
+string skill[3] = {"Pascal(70)","C++(60)","FoxPro(50)"}; 
 
 cout<<"Name   : "<< nm << endl;
 cout<<"Address: "<< add << endl;
 
-for (int i = 0; i < 2; i++)  
+for (i = 0; i < 2; i++)  
         cout << "Hobbies: "<< hb[i] << endl;
 
+cout<<"Married: "<< mar << endl;
+
+for (i=0; i < 3; i++)
+        cout<< "Skill  : "<< skill[i] << endl;
 return 0;
 }
