713741d473820d55b410a36b6ce216e58fee418b
Halim Haidar <halimpiar06@gmail.com>
2019-05-11 17:09:16 +0700

    square

---
+++ 4.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main ()
+{
+    int n=10,i,j;
+   
+
+
+
+        for ( i=0; i<n; i++ )
+        {
+            for (j=0; j<n; j++)
+            {
+                {
+                    printf (" = ");
+                }
+            }
+            printf ("\n");
+        }
+    
+    return 0;
+}
