6957a180972f1480ddfc0f869f77ddd6d89adba1
Halim Haidar <halimpiar06@gmail.com>
2019-05-11 15:24:40 +0700

    i/o

---
+++ 2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+string usrnm, eml;
+
+cout<<"Enter Username : ";
+cin>>usrnm;
+cout<<"Enter e-Mail   : ";
+cin>>eml;
+cout<<"Your Username  : "<<usrnm<<endl;
+cout<<"Your e-Mail    : "<<eml<<endl;
+
+
+
+
+
+
+
+return 0;
+}
