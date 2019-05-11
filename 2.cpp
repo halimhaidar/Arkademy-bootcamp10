df5a552cb066511b9f4931fe2628922ba6554cd1
Halim Haidar <halimpiar06@gmail.com>
2019-05-11 16:12:20 +0700

    final

--- 2.cpp
+++ 2.cpp
@@ -1,17 +1,35 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
     
 string usrnm, eml;
 
+usr:
 cout<<"Enter Username : ";
+
 cin>>usrnm;
+if (usrnm.length()<=8){
+    cout<<"USERNAME VALID"<< endl;}
+else {
+    cout<<"USERNAME TIDAK VALID!!"<< endl;
+    cout<<"======COBA LAGI!======"<< endl;
+    goto usr;}
+eml:
 cout<<"Enter e-Mail   : ";
 cin>>eml;
-cout<<"Your Username  : "<<usrnm<<endl;
-cout<<"Your e-Mail    : "<<eml<<endl;
+if (eml.length()>=4){
+    cout<<"E-MAIL VALID"<<endl;}
+else {
+    
+    cout<<"E-MAIL TIDAK VALID!!"<< endl;
+    cout<<"=====COBA LAGI!====="<< endl;
+    goto eml;}
 
+cout<<"+++++++++++++++++++++++++++++++++++"<<endl;
+cout<<"Your Username  : "<<usrnm<<endl;
+cout<<"Your e-Mail    : "<<eml<<"@mail.com"<<endl;
 
 
 
