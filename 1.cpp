3bfd247011d6173f0223c8460abbd07f7cb0bc24
Halim Haidar <halimpiar06@gmail.com>
2023-02-20 12:50:39 +0700

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,15 +1,15 @@
+
 #include <iostream>
 #include<bits/stdc++.h>
 
 using namespace std;
-
 int main(){
 int i;
 char nm[20]="ABDUL HALIM HAIDAR";
 string add ="Padang/West Sumatera/Indonesia";
 char hb[2] [10]= {"Playing", "Blogging"};
 bool mar=false;
-string skill[3] = {"Pascal(70)","C++(60)","FoxPro(50)"}; 
+string skill[8] = {"Pascal(70)","C++(60)","FoxPro(50)","PHP(50)","HTML,CSS(70)","C#(60)","JavaScript(50)","Java(50)"}; 
 
 cout<<"Name   : "<< nm << endl;
 cout<<"Address: "<< add << endl;
@@ -19,7 +19,7 @@ for (i = 0; i < 2; i++)
 
 cout<<"Married: "<< mar << endl;
 
-for (i=0; i < 3; i++)
+for (i=0; i < 8; i++)
         cout<< "Skill  : "<< skill[i] << endl;
 return 0;
 }
