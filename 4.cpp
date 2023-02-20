fc672b3a770a5f2a164aec715988e62917570e75
Halim Haidar <halimpiar06@gmail.com>
2023-02-20 12:59:35 +0700

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -1,12 +1,14 @@
 #include <stdio.h>
-
+#include <iostream>
+ using namespace std;
 int main ()
 {
-    int n=10,i,j;
-   
-
-
-
+    int n,i,j;
+    
+    cout<<"Membuat Persegi\n";
+    cout<<"---------------\n";
+    cout<<"Masukan Angka untuk Sisi Persegi = ";
+    cin>>n;
         for ( i=0; i<n; i++ )
         {
             for (j=0; j<n; j++)
